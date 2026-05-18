55c555dd645ea8c403bafc0ef502429f7c288022
skye-tachyon <sonicgames772@gmail.com>
2026-05-18 16:35:59 -0400

    kernel: move these logs to KSU_DEBUG
    * these were kind of spammy, anyway if you really
    * don't want logs you should just enable CONFIG_KSU_NOPRINTK
    
    Signed-off-by: //. <174224977+skye-tachyon@users.noreply.github.com>

--- kernel/policy/allowlist.c
+++ kernel/policy/allowlist.c
@@ -401,7 +401,8 @@ out:
 // us to have our own context. we give it a full escaped-to-root one.
 static int persistent_allow_list_pre(void *data)
 {
-	pr_info("do_persistent_allow_list: pid: %d started\n", current->pid);
+	if (IS_ENABLED(CONFIG_KSU_DEBUG))
+		pr_info("do_persistent_allow_list: pid: %d started\n", current->pid);
 
 	/**
 	 * repurpose the mutex they were holding on ksu_persistent_allow_list_fn
@@ -418,7 +419,8 @@ static int persistent_allow_list_pre(void *data)
 
 	mutex_unlock(&allowlist_mutex);
 
-	pr_info("do_persistent_allow_list: pid: %d exit\n", current->pid);
+	if (IS_ENABLED(CONFIG_KSU_DEBUG))
+		pr_info("do_persistent_allow_list: pid: %d exit\n", current->pid);
 	return 0;
 }
 
